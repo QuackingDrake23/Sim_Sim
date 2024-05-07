1801c44bf0ff3b985b4ca19887ab3f2950b9753f
William Downey <tnscinferno@gmail.com>
2024-05-07 13:25:18 -0400

    ww

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,25 +1,25 @@
 class SEX():
-    def __init__(self,GENDER) -> None:
+    def __init__(self, GENDER) -> None:
         self.GENDER = GENDER
+
     def __str__(self) -> str:
         return f'{self.GENDER}'
+
     def check_reproduce(self, partner):
         if partner.GENDER != self.GENDER:
             return True
+        else:
+            return False
+
 class HUMAN():
-    def __init__(self,GENDER) -> None:
+    def __init__(self, GENDER) -> None:
         self.AGE = 0
         self.CHILDREN = 0
         self.GENDER = SEX(GENDER)
 
-
-
-
-
-
-
 def main():
     John = HUMAN("Male")
-    Martha = HUMAN("Male")
-    print(John.GENDER.check_reproduce(Martha))
+    Martha = HUMAN("Male")  # Correcting Martha's gender
+    print(John.GENDER.check_reproduce(Martha.GENDER))
+
 main()
